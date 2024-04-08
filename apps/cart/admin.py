db98fac77d4d437644e88adf7c55cfb43ba5305a
Aboubacar Sawadogo <aboubacarsawadogo@Aboubacars-MacBook-Pro.local>
2024-04-08 18:52:16 +0000

    methode de commande revue

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Sales, Cart, CartProduct, Order, Product, Category
+# from .models import Sales, Cart, CartProduct, Order, Product, Category,ProductImagesURL
+from .models import Order
 
 
-admin.site.register([Sales, Cart, CartProduct, Order, Product, Category])
-# Register your models here.
+# admin.site.register([Sales, Cart, CartProduct, Order, Product,ProductImagesURL, Category])
+# # Register your models here.
+
+admin.site.register([Order])
+
 
